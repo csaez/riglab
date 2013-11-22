626d5a408582b56389e9cceec68e5a6e912e3436
csaez <cesarte@gmail.com>
2013-11-22 00:26:45 -0300

    pose reader draft

---
+++ riglab/psd/reader.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import os
+from wishlib.si import si, siget  # , SIWrapper
+
+COMPOUND_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__),
+                                             "..", "data", "compounds"))
+
+
+class PoseReader(object):
+
+    def __init__(self, obj):
+        super(PoseReader, self).__init__()
+        self.obj = obj
+
+
+class ConeReader(PoseReader):
+    COMPOUND_FILE = "riglab__ConeReader.xsicompound"
+    COMPOUND = os.path.splitext(COMPOUND_FILE)[0]
+    param_table = {"angle": "Angle",
+                   "size": "Size",
+                   "debug": "Debug",
+                   "show_value": "Show_Value"}
+
+    def __init__(self, *args, **kwds):
+        super(ConeReader, self).__init__(*args, **kwds)
+        self._ICE = self.obj.ActivePrimitive.ICETrees(self.COMPOUND)
+        if not self._ICE:
+            msj = "{} isnt a valid reader, operator not found."
+            raise Exception(msj.format(self.obj))
+
+    @classmethod
+    def new(cls, target=None):
+        obj = si.SIGetPrim("PointCloud")("Value")  # create an empty pcloud
+        obj.Name = "poseReader"
+        si.ApplyICEOp(os.path.join(COMPOUND_DIR, cls.COMPOUND_FILE), obj, " ")
+        reader = cls(obj)
+        if target:
+            reader.target = target
+            # align to target
+            reader.obj.Kinematics.Global.Transform = target.Kinematics.Global.Transform
+        return reader
+
+    def __setattr__(self, attr, value):
+        super(ConeReader, self).__setattr__(attr, value)
+        param = self.param_table.get(attr)
+        if param:
+            self._setvalue(param, value)
+
+    def __getattr__(self, attr):
+        param = self.param_table.get(attr)
+        if param:
+            return self._getvalue(param)
+        return super(ConeReader, self).__getattr__(attr)
+
+    @property
+    def target(self):
+        value = self._getvalue("SceneReferenceNode.reference", False)
+        return siget(value)
+
+    @target.setter
+    def target(self, value):
+        self._setvalue("SceneReferenceNode.reference", value.FullName, False)
+
+    def show(self):
+        si.InspectObj(".".join([self._ICE.FullName, self.COMPOUND]))
+
+    def _setvalue(self, param, value, compound=True):
+        base = [self._ICE.FullName]
+        if compound:
+            base.append(self.COMPOUND)
+        base.append(param)
+        siget(".".join(base)).Value = value
+
+    def _getvalue(self, param, compound=True):
+        base = [self._ICE.FullName]
+        if compound:
+            base.append(self.COMPOUND)
+        base.append(param)
+        return siget(".".join(base)).Value
