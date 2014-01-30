7a7ac2e377014664bbed8efb0d25b75328f20b80
csaez <cesarte@gmail.com>
2014-01-30 03:21:52 -0300

    License GPL3

--- riglab/utils.py
+++ riglab/utils.py
@@ -1,9 +1,24 @@
+# This file is part of riglab.
+# Copyright (C) 2014  Cesar Saez
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 from math import pi
 from collections import namedtuple
 
 import naming
-from wishlib.si import si, C, sisel, siget, project_into_mesh, simath
+from wishlib.si import si, sisel, project_into_mesh, simath
 
 from .joint import Joint
 
