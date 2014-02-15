ff8e55aa8fc0a422e7f600a221b90fb16f8cfcd3
csaez <cesarte@gmail.com>
2014-02-15 16:52:26 -0300

    typo

--- riglab_plugin.py
+++ riglab_plugin.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from wishlab.qt import QtGui
+from wishlib.qt import QtGui
 from wishlib.si import si, sisel, log, C, show_qt
 
 
