cfadb517491e24baeca777278a96b9259c73a217
csaez <cesarte@gmail.com>
2015-04-01 00:54:46 +1100

    naming profiles added

--- riglab/naming.py
+++ riglab/naming.py
@@ -1,7 +1,30 @@
+PROFILES = {"current": ""}
 TOKENS = dict()
 
 
-def get_tokens():
+class Profile(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.fields = list()
+        self.separator = "_"
+
+
+class Field(object):
+
+    def __init__(self, name):
+        self.name = name
+        self._value = ""
+        self.tokens = list()
+        self.default = None
+
+    def append_token(self, token, default=False):
+        self.tokens.append(token)
+        if default:
+            self.default = token
+
+
+def list_tokens():
     return TOKENS.keys()
 
 
@@ -25,3 +48,49 @@ def delete_token(name):
 
 def clear_tokens():
     TOKENS.clear()
+
+
+def list_profiles():
+    return [x for x in PROFILES.keys() if x != "current"]
+
+
+def new_profile(name):
+    if PROFILES.get(name):
+        return None
+    PROFILES[name] = Profile(name)
+    return PROFILES[name]
+
+
+def get_profile(name):
+    p = PROFILES.get(name)
+    if p and name == "current":
+        return PROFILES.get(p)
+    return p
+
+
+def set_profile(name):
+    if PROFILES.get(name):
+        PROFILES["current"] = name
+        return True
+    return False
+
+
+def current_profile():
+    name = PROFILES["current"]
+    p = PROFILES.get(name)
+    if p:
+        return p
+    PROFILES["current"] = ""
+    return None
+
+
+def delete_profile(name):
+    if name == "current":
+        name = PROFILES[name]
+    if PROFILES.get(name):
+        del PROFILES[name]
+        ps = list_profiles()
+        if len(ps):
+            set_profile(ps[0])
+        else:
+            PROFILES["current"] = ""
--- riglab/tests/test_naming.py
+++ riglab/tests/test_naming.py
@@ -4,16 +4,16 @@ from riglab import naming as n
 
 class NamingTests(unittest.TestCase):
 
-    def test_get_tokens(self):
+    def test_list_tokens(self):
         n.clear_tokens()
-        self.assertEqual(len(n.get_tokens()), 0)
+        self.assertEqual(len(n.list_tokens()), 0)
 
     def test_add_tokens(self):
         n.clear_tokens()
         self.assertTrue(n.new_token("name", "value1"))
         self.assertFalse(n.new_token("name", "value2"))
-        self.assertEqual(len(n.get_tokens()), 1)
-        self.assertEqual(n.get_tokens(), ["name"])
+        self.assertEqual(len(n.list_tokens()), 1)
+        self.assertEqual(n.list_tokens(), ["name"])
 
     def test_get_token(self):
         n.clear_tokens()
@@ -26,6 +26,20 @@ class NamingTests(unittest.TestCase):
         self.assertFalse(n.delete_token("foo"))
         n.new_token("foo", "bar")
         self.assertTrue(n.delete_token("foo"))
+
+    def test_list_profile(self):
+        self.assertIsNotNone(n.list_profiles())
+
+    def test_new_profile(self):
+        self.assertIsNotNone(n.new_profile("test"))
+        self.assertIn("test", n.list_profiles())
+        self.assertIsNotNone(n.get_profile("test"))
+        self.assertTrue(n.set_profile("test"))
+        self.assertEqual(n.current_profile().name, "test")
+
+        for p in n.list_profiles():
+            n.delete_profile(p)
+        self.assertEqual(len(n.list_profiles()), 0)
 
 
 if __name__ == "__main__":
